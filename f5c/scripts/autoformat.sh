83fa33e0dd84f1acb764c90877d383dc5e4b87c1
dkhyland <davidh7057@gmail.com>
2020-04-03 12:19:31 +1100

    removed f5c

--- f5c/scripts/autoformat.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-# Apply clang format to all source files
-
-clang-format -i *.c *.h *.cu *.cuh
--- f5c/scripts/bootstrap.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-autoreconf
-./scripts/install-hts.sh
-./scripts/install-hdf5.sh
-./configure --enable-localhdf5
